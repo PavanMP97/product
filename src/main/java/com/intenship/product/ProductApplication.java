14aaac7a1ee49cccac555d4129cd00cfa54d0a2d
pp_0271 <mppa1mp@gmail.com>
2022-09-16 11:16:47 +0530

    done

--- src/main/java/com/intenship/product/ProductApplication.java
+++ src/main/java/com/intenship/product/ProductApplication.java
@@ -4,11 +4,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
 @EnableEurekaClient
 public class ProductApplication {
 
+	@Bean
+	public WebClient.Builder getWebClientBuilder(){
+		return WebClient.builder();
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(ProductApplication.class, args);
 	}
--- src/main/java/com/intenship/product/repository/ProductRepository.java
+++ src/main/java/com/intenship/product/repository/ProductRepository.java
@@ -2,8 +2,16 @@ package com.intenship.product.repository;
 
 import com.intenship.product.entity.ProductDomain;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import javax.transaction.Transactional;
 
 @Repository
 public interface ProductRepository extends JpaRepository<ProductDomain,Long> {
+    @Modifying
+    @Transactional
+    @Query(value = "update product_table set stock=?1 where product_id=?2",nativeQuery = true)
+    void updateStockById(int stock,Long productId);
 }
